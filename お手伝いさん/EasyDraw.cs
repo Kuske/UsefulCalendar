5b565af8cb31b71023c5ea6b92db57d40464cf0d
Kuske <lucky.777yusuke@gmail.com>
2017-04-14 01:57:18 +0900

    SetPlan : 丸の塗りつぶし描画の追加 ・丸が塗りつぶし描画できるようになったよ

--- お手伝いさん/EasyDraw.cs
+++ お手伝いさん/EasyDraw.cs
@@ -16,11 +16,11 @@ namespace Kuske.UsefulCalender.DrawGeometry
     // 線を描画する
     static class Line
     {
-        public static void DrawLine(Graphics DrawGraphics, int Thickness, Color LineColor, int x1, int y1, int x2, int y2)
+        public static void DrawLine(Graphics DrawGraphics, int Thickness, Color DrawColor, int x1, int y1, int x2, int y2)
         {
 
             //pen作成
-            Pen WritePen = new Pen(LineColor, Thickness);
+            Pen WritePen = new Pen(DrawColor, Thickness);
 
             //lineの始点と終点
             Point Start_point1 = new Point(x1, y1);
@@ -37,10 +37,10 @@ namespace Kuske.UsefulCalender.DrawGeometry
     // 四角を描画する
     static class Box
     {
-        public static void DrawBox(Graphics DrawGraphics, int Thickness, Color LineColor, int x1, int y1, int x2, int y2, bool FillFlag)
+        public static void DrawBox(Graphics DrawGraphics, int Thickness, Color DrawColor, int x1, int y1, int x2, int y2, bool FillFlag)
         {
             //pen作成
-            Pen WritePen = new Pen(LineColor, Thickness);
+            Pen WritePen = new Pen(DrawColor, Thickness);
 
             //外枠を描画
             DrawGraphics.DrawRectangle(WritePen, Math.Min(x1, x2), Math.Min(y1, y2), Math.Max(x1, x2), Math.Max(y1, y2));
@@ -48,10 +48,14 @@ namespace Kuske.UsefulCalender.DrawGeometry
             // 塗りつぶし処理
             if (FillFlag == true)
             {
-                for (int a = Math.Min(x1, x2); a <= Math.Max(x1, x2); a++)
-                {
-                    DrawGraphics.DrawLine(WritePen, a, Math.Max(y1, y2), a, Math.Max(y1, y2));
-                }
+                // ブラシを用意
+                SolidBrush DrawBrash = new SolidBrush(DrawColor);
+
+                // 塗りつぶす
+                DrawGraphics.FillRectangle(DrawBrash, Math.Min(x1, x2), Math.Min(y1, y2), Math.Max(x1, x2), Math.Max(y1, y2));
+
+                // ブラシの消去
+                DrawBrash.Dispose();
             }
 
             //penを解放
@@ -63,21 +67,28 @@ namespace Kuske.UsefulCalender.DrawGeometry
     static class Circle
     {
         // 楕円を描画
-        public static void DrawEllipse(Graphics DrawGraphics, int Thickness, Color LineColor, int x1, int y1, int x2, int y2, bool FillFlag)
+        public static void DrawEllipse(Graphics DrawGraphics, int Thickness, Color DrawColor, int x1, int y1, int x2, int y2, bool FillFlag)
         {
-            //pen作成
-            Pen WritePen = new Pen(LineColor, Thickness);
+            // pen作成
+            Pen WritePen = new Pen(DrawColor, Thickness);
 
             DrawGraphics.DrawEllipse(WritePen, Math.Min(x1, x2), Math.Min(y1, y2), Math.Max(x1, x2), Math.Max(y1, y2));
 
-            // FillFlagがtrueだったら
+            // 塗りつぶし処理
             if(FillFlag == true)
             {
+                // ブラシを用意
+                SolidBrush DrawBrash = new SolidBrush(DrawColor);
+
                 // 塗りつぶす
+                DrawGraphics.FillEllipse(DrawBrash, Math.Min(x1, x2), Math.Min(y1, y2), Math.Max(x1, x2), Math.Max(y1, y2));
 
+                // ブラシの消去
+                DrawBrash.Dispose();
             }
-
-
+            
+            //penを解放
+            WritePen.Dispose();
         }
     }
 }
